82ea1d5f14b4ad60f31c503d4cf6a76fc8b90887
Peter Juhasz <jpeter82@gmail.com>
2017-11-20 10:31:04 +0100

    first strategy

--- player.py
+++ player.py
@@ -3,8 +3,14 @@ class Player:
     VERSION = "c2h5oh ver1"
 
     def betRequest(self, game_state):
-        return game_state['current_buy_in']
+        bet = 0
+        if game_state['bet_index'] == 0:
+            bet = game_state['small_blind']
+        elif game_state['bet_index'] == 1:
+            bet = game_state['small_blind'] * 2
+        else:
+            bet = game_state['current_buy_in']
+        return bet
 
     def showdown(self, game_state):
         pass
-
