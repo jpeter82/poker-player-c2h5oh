7ba6f21d3837ecc5d77cbecbd6f801fc1bce94c2
Péter Biró <biro.peter.hu@gmail.com>
2017-11-20 13:55:31 +0100

    separate turn logic functions

--- player.py
+++ player.py
@@ -1,19 +1,39 @@
 
 class Player:
     VERSION = "c2h5oh ver1"
+  
+    # first bet round without communnity cards
+    def first_round(self, game_state):
+        if game_state['bet_index'] == 0:
+            bet = game_state['small_blind']
+        elif game_state['bet_index'] == 1:
+            bet = game_state['small_blind'] * 2
+        else:
+            bet = game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+        return bet
 
+    # 3 community cards
+    def second_round(self, game_state):
+        return game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+
+    # 4 community cards
+    def third_round(self, game_state):
+        return game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+
+    # 5 community cards
+    def fourth_round(self, game_state):
+        return game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+    
     def betRequest(self, game_state):
-
         bet = 0
         if len(game_state['community_cards']) == 0:
-            if game_state['bet_index'] == 0:
-                bet = game_state['small_blind']
-            elif game_state['bet_index'] == 1:
-                bet = game_state['small_blind'] * 2
-            else:
-                bet = game_state['current_buy_in']
-        else:
-               bet = game_state['current_buy_in']    
+            bet = self.first_round(game_state)
+        elif len(game_state['community_cards']) == 3:
+            bet = self.second_round(game_state)
+        elif len(game_state['community_cards']) == 4:
+            bet = self.third_round(game_state)
+        elif len(game_state['community_cards']) == 5:
+            bet = self.fourth_round(game_state) 
         return bet
 
     def showdown(self, game_state):
