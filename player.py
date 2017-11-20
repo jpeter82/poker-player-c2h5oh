2d138782082edab509471563c9810fc78444cd54
Péter Biró <biro.peter.hu@gmail.com>
2017-11-20 14:22:14 +0100

    sep functions again

--- player.py
+++ player.py
@@ -1,20 +1,46 @@
-
 class Player:
-    VERSION = "c2h5oh ver1"
+    VERSION = "c2h5oh ver2"
+  
+    # first bet round without communnity cards
+    def first_round(self, game_state):
+        if game_state['bet_index'] == 0:
+            bet = game_state['small_blind']
+        elif game_state['bet_index'] == 1:
+            bet = game_state['small_blind'] * 2
+        else:
+            # player_idx = game_state['in_action']
+            # bet = game_state['current_buy_in'] - game_state['players'][player_idx]['bet']
+            bet = game_state['current_buy_in']
+        return bet
 
+    # 3 community cards
+    def second_round(self, game_state):
+        # bet = game_state['current_buy_in'] - game_state['players'][player_idx]['bet']
+        bet = game_state['current_buy_in']
+        return bet
+
+    # 4 community cards
+    def third_round(self, game_state):
+        # bet = game_state['current_buy_in'] - game_state['players'][player_idx]['bet']
+        bet = game_state['current_buy_in']
+        return bet
+
+    # 5 community cards
+    def fourth_round(self, game_state):
+        # bet = game_state['current_buy_in'] - game_state['players'][player_idx]['bet']
+        bet = game_state['current_buy_in']
+        return bet
+    
     def betRequest(self, game_state):
         bet = 0
         if len(game_state['community_cards']) == 0:
-            if game_state['bet_index'] == 0:
-                bet = game_state['small_blind']
-            elif game_state['bet_index'] == 1:
-                bet = game_state['small_blind'] * 2
-            else:
-                bet = game_state['current_buy_in']
-                # bet = game_state['current_buy_in'] - game_state['players']['in_action']['bet']
-        else:
-            bet = game_state['current_buy_in']
-            # bet = game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+            bet = self.first_round(game_state)
+        elif len(game_state['community_cards']) == 3:
+            bet = self.second_round(game_state)
+        elif len(game_state['community_cards']) == 4:
+            bet = self.third_round(game_state)
+        elif len(game_state['community_cards']) == 5:
+            bet = self.fourth_round(game_state) 
         return bet
 
     def is_pair_in_hand(self, game_state):
